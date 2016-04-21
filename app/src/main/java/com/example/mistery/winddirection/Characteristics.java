d325536b05d1ec1b82dfaea86d116574bb90415b
ZakharchnkoWork <Zakharchenkowork@gmail.com>
2016-04-21 13:00:39 +0300

    Characteristics class refactoring
    Change formatting and comments

--- app/src/main/java/com/example/mistery/winddirection/Characteristics.java
+++ app/src/main/java/com/example/mistery/winddirection/Characteristics.java
@@ -8,30 +8,45 @@ import com.znshadows.auxilary.Math2DVector;
 /**
  * Created by MisterY on 10.12.2015.
  */
-public class Characteristics extends Math2DVector{
+public class Characteristics extends Math2DVector {
 
 
-    private float course;
-    private float speed;
+    private float course; //course in degrees
+    private float speed; //speed in Knots
 
+    //used for checks, is the objects is filled allready
     private boolean isCourseSet = false;
     private boolean isSpeedSet = false;
 
 
-
+    /**
+     * Use if Values are known in time of creating of the object
+     *
+     * @param course in Degrees
+     * @param speed  in Knots
+     */
     public Characteristics(float course, float speed) {
-        super(course,speed);
+        super(course, speed);
         this.course = course;
         this.speed = speed;
         isCourseSet = true;
         isSpeedSet = true;
     }
 
+    /**
+     * Empty object, marked as nonfilled
+     */
     public Characteristics() {
-        super(0,0);
+        super(0, 0);
         isCourseSet = false;
         isSpeedSet = false;
     }
+
+    /**
+     * in case of fill characteristics from vector
+     *
+     * @param vector
+     */
     private Characteristics(Math2DVector vector) {
         super(vector);
         this.course = vector.getDirection();
@@ -39,21 +54,29 @@ public class Characteristics extends Math2DVector{
         isCourseSet = true;
         isSpeedSet = true;
     }
+
+    /**
+     * for setting or changing course
+     *
+     * @param course in Degrees
+     */
     public void setCourse(float course) {
         this.course = course;
         isCourseSet = true;
-
-        if(isSpeedSet())
-        {
+        if (isSpeedSet()) {
             setCoordinates(convertToCoordinates(course, speed));
         }
     }
 
+    /**
+     * for setting or changing speed
+     *
+     * @param speed in knots
+     */
     public void setSpeed(float speed) {
         this.speed = speed;
         isSpeedSet = true;
-        if(isCourseSet())
-        {
+        if (isCourseSet()) {
             setCoordinates(convertToCoordinates(course, speed));
         }
     }
@@ -76,21 +99,25 @@ public class Characteristics extends Math2DVector{
 
 
     /**
-     * if course and speed were set, then Charasteristic is ready
-     * @return true if course and speed were set
+     * In case of any calculations Characteristics shold be ready
+     *
+     * @return true if course and speed were set already
      */
     public boolean isReady() {
-        if(isCourseSet && isSpeedSet)
-        {return true;}
-        else {
+        if (isCourseSet && isSpeedSet) {
+            return true;
+        } else {
             return false;
         }
     }
 
 
     /**
+     * For calculation of the true wind characteristics, from the values of the vessel's characteristics
+     * and wind characteristics
+     *
      * @param shipsCharacteristics prepared characteristics of Vessel
-     * @param windCharacteristics prepared characteristics of Wind
+     * @param windCharacteristics  prepared characteristics of Wind
      * @return True Wind Characteristics
      */
     public static Characteristics calculateTrueWind(Characteristics shipsCharacteristics, Characteristics windCharacteristics) {
